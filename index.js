366957bed40193dd2eb6018f2ddd67fb5d9602c3
Othmane Elkaabi <79403639+moi-brooks@users.noreply.github.com>
2024-12-18 20:31:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import { generateTextAndImage } from "./utils.js"
+
+
+let name = "Othmane El Kaabi"
+
+
+let favoriteActivity = "football"
+
+
+let favoritePlace = "soccer stadium"
+
+// The higher temperature, the more random & experimental output
+let temperature = 1
+
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
